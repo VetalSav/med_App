b945b6160607a811049f720d1d25ee3e5a3e66bf
Vitali <110376993+VetalSav@users.noreply.github.com>
2022-10-29 21:48:01 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
+import {LaptopOutlined, NotificationOutlined, UserOutlined} from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import {Breadcrumb, Layout, Menu} from 'antd';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const {Header, Content, Footer, Sider} = Layout;
+
+const items1: MenuProps['items'] = ['1', '2', '3', '4'].map(key => ({
+    key,
+    label: `nav ${key}`,
+}));
+
+const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+    (icon, index) => {
+        const key = String(index + 1);
+
+        return {
+            key: `sub${key}`,
+            icon: React.createElement(icon),
+            label: `subnav ${key}`,
+
+            children: new Array(4).fill(null).map((_, j) => {
+                const subKey = index * 4 + j + 1;
+                return {
+                    key: subKey,
+                    label: `option${subKey}`,
+                };
+            }),
+        };
+    },
+);
+
+const App: React.FC = () => (
+    <Layout>
+        <Header className="header">
+            <div className="logo"/>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} items={items1}/>
+        </Header>
+        <Content style={{padding: '0 50px'}}>
+            <Breadcrumb style={{margin: '16px 0'}}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Layout className="site-layout-background" style={{padding: '24px 0'}}>
+                <Sider className="site-layout-background" width={200}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{height: '100%'}}
+                        items={items2}
+                    />
+                </Sider>
+                <Content style={{padding: '0 24px', minHeight: 280}}>Content</Content>
+            </Layout>
+        </Content>Hello bro
+        <Footer style={{textAlign: 'center'}}>Vetal Design ©2022</Footer>
+    </Layout>
+);
 
 export default App;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { Layout } from 'antd';
+import React from 'react';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+export const LayuotCom: React.FC = () => {
+    return( <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+    </Layout>)
+
+}
+
+
+
+
